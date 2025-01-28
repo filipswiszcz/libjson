45de0caf719757317cdba80f57a9725c1d31e581
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-01-28 23:49:52 +0100

    fucking with aho-corasick

--- src/libjson.c
+++ src/libjson.c
@@ -1,1 +1,17 @@
 #include "libjson.h"
+
+
+json_object json_object_init(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("[libjson] %s could not be found", filename); // return null
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    while (getline(&line, &len, file) != -1) {
+        
+    }
+
+    // json_object *object = calloc(1, sizeof(json_object));
+}
--- src/libjson.h
+++ src/libjson.h
@@ -1,8 +1,40 @@
 #ifndef LIBJSON_H
 #define LIBJSON_H
 
+#include <stdio.h>
+
+
+enum type {
+    STR,
+    INT,
+    FLOAT,
+    ARR,
+};
+
+
+//                      [json object]
+//                  |                   |
+//              []
+
+// tree
+
+typedef struct object_node object_node;
+
+struct object_node {
+    char *c;
+    object_node *next;
+};
+
 typedef struct json_object json_object;
 
-struct json_object {};
+struct json_object {
+    char *cont;
+    object_node next_c[10]; // fuck it, i need unlimited arr
+    json_object *next_obj;
+};
+
+#define get_json_object(filename) json_object_init(filename)
+
+json_object json_object_init(const char *filename);
 
 #endif
