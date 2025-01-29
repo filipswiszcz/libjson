ce0dcc0ab44e7d0e893c9fb0e78c78a2754299ab
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-01-29 23:06:12 +0100

    dynamic array

--- src/libjson.c
+++ src/libjson.c
@@ -1,17 +1,46 @@
 #include "libjson.h"
 
 
-json_object json_object_init(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        printf("[libjson] %s could not be found", filename); // return null
-    }
+// json array
 
-    char *line = NULL;
-    size_t len = 0;
-    while (getline(&line, &len, file) != -1) {
-        
-    }
-
-    // json_object *object = calloc(1, sizeof(json_object));
+void json_array_append(json_array *arr, int n) {
+    do {
+        if (arr -> k >= arr -> capacity) {
+            if (arr -> capacity == 0) arr -> capacity = 256;
+            else arr -> capacity *= 2;
+            arr -> nums = realloc(arr -> nums, arr -> capacity * sizeof(*arr -> nums));
+        }
+        arr -> nums[arr -> k++] = n;
+    } while (0);
 }
+
+// json object
+
+// json_object json_object_init(const char *filename) {
+//     FILE *file = fopen(filename, "r");
+//     if (!file) {
+//         printf("[libjson] %s could not be found", filename); // return null
+//     }
+
+//     // object_node node;
+
+//     // char *line = NULL;
+//     // size_t len = 0;
+//     // int sw = 0;
+//     // while (getline(&line, &len, file) != -1) {
+//     //     for (int i = 0; i < len; i++) {
+//     //         if (line[i] == '{') continue;
+//     //         else if (line[i] == '"') sw = 1;
+//     //         // if (sw) node
+//     //     }
+//     // }
+
+//     // json_object *object = calloc(1, sizeof(json_object));
+// }
+
+// void append_node(char *line, object_node *node, int i) {
+//     if (line[i] == ':') {
+//         node -> val = "val"; return;
+//     }
+//     node -> c = line[i];
+// }
--- src/libjson.h
+++ src/libjson.h
@@ -2,7 +2,22 @@
 #define LIBJSON_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
+
+// json array
+
+typedef struct json_array json_array;
+
+struct json_array {
+    int *nums;
+    size_t k;
+    size_t capacity;
+};
+
+void json_array_append(json_array *arr, int n);
+
+// json object
 
 enum type {
     STR,
@@ -18,23 +33,28 @@ enum type {
 
 // tree
 
-typedef struct object_node object_node;
+// map can be sorted starting with chars of most used words
 
-struct object_node {
-    char *c;
-    object_node *next;
-};
+// int A = 0, B = 1, C = 2, D = 3;
 
-typedef struct json_object json_object;
+// typedef struct object_node object_node;
 
-struct json_object {
-    char *cont;
-    object_node next_c[10]; // fuck it, i need unlimited arr
-    json_object *next_obj;
-};
+// struct object_node {
+//     char c;
+//     char *val;
+//     // object_node arr[4];
+// };
 
-#define get_json_object(filename) json_object_init(filename)
+// typedef struct json_object json_object;
 
-json_object json_object_init(const char *filename);
+// struct json_object {
+//     char *cont;
+//     // object_node next_c[10]; // fuck it, i need unlimited arr or actually dont
+//     json_object *next_obj;
+// };
+
+// #define get_json_object(filename) json_object_init(filename)
+
+// json_object json_object_init(const char *filename);
 
 #endif
